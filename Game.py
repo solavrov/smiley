c008a5c5a42fe6a76cbd2912fc85787a7a8cc735
solavrov <2slavrov@gmail.com>
2018-08-10 17:32:44 +0300

    some bugs

--- Game.py
+++ Game.py
@@ -15,20 +15,20 @@ class Game(App):
 
         Window.size = (600, 600)
 
-        folder = 'images'
+        folder = 'images/'
 
         gt = GameTimer(color=[1, 0, 1, 1])
         h = Avatar(x=275, y=275, v=300, size=50,
-                   face=folder + '/avatar.png', moving_face=folder + '/move.png', hit_face=folder + '/catch.png')
+                   face=folder + 'avatar.png', moving_face=folder + 'move.png', hit_face=folder + 'catch.png')
 
         s = Stage(gt, h)
-        s.set_background(Background(folder + '/background.png'))
+        s.set_background(Background(folder + 'back.png'))
         s.show_hero()
 
         for i in range(5):
             angle = random() * 2 * pi
             a = Momo(x=random() * 150, y=random() * 150, vx=100 * sin(angle), vy=100 * cos(angle), a=0.05, size=50,
-                     happy_face=folder + '/momo.png', sad_face=folder + '/hit.png', sad_time=0.4, speed_lim=750)
+                     happy_face=folder + 'momo.png', sad_face=folder + 'hit.png', sad_time=0.4, speed_lim=750)
             s.add(a)
 
         s.show_game_timer()
